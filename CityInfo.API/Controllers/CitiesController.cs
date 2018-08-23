931e4b2a30ec10e2fb9049381005e29c83d61bde
Amit <amit.k15@outlook.com>
2018-08-23 23:15:49 +0530

    Improving the architecture

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,18 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CityInfo.API.Controllers
 {
+    [Route("api/cities")]
     public class CitiesController : Controller
     {
-        [HttpGet("api/cities")]
-        public IActionResult GetCities()
+        [HttpGet()]
+        public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>()
-            {
-                new { id=1, Name="Bangalore"},
-                new { id=2, Name="Muzaffarpur"}
-            });
+            //return new JsonResult(new List<object>()
+            //{
+            //    new { id=1, Name="Bangalore"},
+            //    new { id=2, Name="Muzaffarpur"}
+            //});
+            return new JsonResult(CitiesDataStore.Current.Cities);
         }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+        }
+
     }
 }
