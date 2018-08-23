70e6469b7ba1c98cda0bae9a049d2081195929b4
Amit <amit.k15@outlook.com>
2018-08-23 22:53:31 +0530

    Returning Resources

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CityInfo.API.Controllers
+{
+    public class CitiesController : Controller
+    {
+        [HttpGet("api/cities")]
+        public IActionResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id=1, Name="Bangalore"},
+                new { id=2, Name="Muzaffarpur"}
+            });
+        }
+    }
+}
